ea9b7bd6f1159e3c815e979ee04370c04fd46af8
Marte Roel <marteroel@users.noreply.github.com>
2018-11-19 16:45:08 +0100

    Added Unity project and package

---
+++ Audio Player/Assets/AudioPlayer/Scripts/AudioPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+	public AudioSource[] clips;
+	public string[] keyToTriggerAudio;
+	public bool useKeyInput;
+	public bool useNumberKeys;
+
+
+	void Awake () {
+		foreach (AudioSource clip in clips) {
+			clip.Stop(); //dissables play on awake
+		
+		}
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		if (useKeyInput)
+			PlayAudioFromKeys ();
+	}
+
+	private void PlayAudioFromKeys(){
+		
+		if(useNumberKeys){
+			for (int i = 0; i < clips.Length; i++) {
+				if(Input.GetKeyDown((i+1).ToString()))
+					clips[i].Play();
+			}
+		}
+
+		else {
+			for (int i = 0; i < clips.Length; i++) {
+				if (Input.GetKeyDown (keyToTriggerAudio [i]))
+					clips [i].Play ();
+			}
+		}
+	}
+
+	public void TriggerAudio(int audioIndex){		
+		clips [audioIndex].Play ();
+	}
+}
